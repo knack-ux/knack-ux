af930889bed568023c32162b4c751a2cb5c389a5
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2019-09-26 23:35:07 +0200

    fix(button): add styled.tsx for styled-components
    #1

--- packages/Button/src/index.tsx
+++ packages/Button/src/index.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const Primary = styled.button`
-  font-family: "Open Sans";
-  font-size: 18px;
-  font-weight: 600;
-  background-color: #3C4290;
-  border: none;
-  color: white;
-  padding: 12px 24px;
-  border-radius: 3px;
-  cursor: pointer;
-  transition-property: background-color;
-  transition-duration: .3s;
-  -webkit-font-smoothing: antialiased;
-
-  :hover {
-    background-color: #5F66D7;
-  }
-
-  :active {
-    background-color: #21244B;
-  }
-`;
-
-const Secondary = styled(Primary)`
-  padding: 8px 24px;
-  font-size: 14px;
-`;
+import { Primary, Secondary } from './styled';
 
 type ReactButton = React.ButtonHTMLAttributes<HTMLButtonElement>;
-interface Props extends ReactButton{
+interface Props extends ReactButton {
   primary?: boolean
   secondary?: boolean
 }
