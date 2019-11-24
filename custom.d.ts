2b922ad2f79661e87f88df3bbbff2e83073ad294
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2019-11-24 23:10:27 +0100

    fix(input): typings

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,3 @@
-/* eslint import/export: 0 */
-import 'styled-components';
-
 declare module '*.svg' {
   const content: any;
   export default content;
@@ -14,16 +11,4 @@ declare module '*.png' {
 declare module '*.json' {
   const content: any;
   export default content;
-}
-
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      default: string
-      success: string
-      warning: string
-      danger: string
-    },
-    focus: string
-  }
 }
--- packages/ThemeProvider/src/index.tsx
+++ packages/ThemeProvider/src/index.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { ThemeProvider as Base, DefaultTheme } from 'styled-components';
 import _get from 'lodash.get';
 
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      default: string
+      success: string
+      warning: string
+      danger: string
+    },
+    focus: string
+  }
+}
+
 export const defaultTheme = {
   colors: {
     default: '#0052CC',
