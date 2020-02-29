2bd445b2431e912a2034b3453d3c417c89407188
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2020-02-29 13:18:02 +0100

    refactor(checkbox): move styled-components to styled.tsx

--- packages/Checkbox/src/index.tsx
+++ packages/Checkbox/src/index.tsx
@@ -1,6 +1,11 @@
 import React, { ChangeEvent, ComponentPropsWithoutRef } from 'react';
-import styled from 'styled-components';
 import Icon from '@knack-ux/icon';
+
+import {
+  CheckboxBase,
+  Input,
+  Label
+} from './styled';
 
 export interface Option {
   label: string
@@ -47,52 +52,6 @@ export function Checkbox({
     </CheckboxBase>
   );
 }
-
-export const CheckboxBase = styled.div`
-  display: inline-flex;
-  flex-direction: column;
-
-  font-family: 'IBM Plex Sans';
-`;
-
-export const Input = styled.input`
-  position: absolute;
-  clip: rect(1px,1px,1px,1px);
-  height: 1px;
-  width: 1px;
-  opacity: 0;
-  overflow: hidden;
-`;
-
-export const Label = styled.label<{checked?: boolean}>`
-  display: inline-flex;
-  align-items: center;
-  cursor: pointer;
-
-  & > i {
-    user-select: none;
-    box-sizing: border-box;
-
-    font-size: 16px;
-  
-    border-radius: 3px;
-    background: ${({ checked, theme }) => (checked ? theme.colors.info : '#FFFFFF')};
-    border: 2px solid ${({ theme }) => theme.colors.info};
-
-    margin-right: 16px;
-  }
-
-  &[aria-disabled="true"] {
-    & > i {
-      border: 2px solid #B3BAC5; 
-      background: ${({ checked }) => (checked ? '#B3BAC5' : '#FFF')};
-    }
-  }
-
-  &:nth-of-type(n + 2) {
-    margin-top: 8px;
-  }
-`;
 
 
 export default Checkbox;
---
+++ packages/Checkbox/src/styled.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const CheckboxBase = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+
+  font-family: 'IBM Plex Sans';
+`;
+
+export const Input = styled.input`
+  position: absolute;
+  clip: rect(1px,1px,1px,1px);
+  height: 1px;
+  width: 1px;
+  opacity: 0;
+  overflow: hidden;
+`;
+
+export const Label = styled.label<{checked?: boolean}>`
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+
+  & > i {
+    user-select: none;
+    box-sizing: border-box;
+
+    font-size: 16px;
+  
+    border-radius: 3px;
+    background: ${({ checked, theme }) => (checked ? theme.colors.info : '#FFFFFF')};
+    border: 2px solid ${({ theme }) => theme.colors.info};
+
+    margin-right: 16px;
+  }
+
+  &[aria-disabled="true"] {
+    & > i {
+      border: 2px solid #B3BAC5; 
+      background: ${({ checked }) => (checked ? '#B3BAC5' : '#FFF')};
+    }
+  }
+
+  &:nth-of-type(n + 2) {
+    margin-top: 8px;
+  }
+`;
